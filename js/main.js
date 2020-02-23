68f2aff194eae59379eb89d8901f0fbf59fcc4d3
Dmitry <maildima1101@gmail.com>
2020-02-23 16:58:12 +0300

    Task7 done

---
+++ js/main.js
@@ -0,0 +1,63 @@
+'use strict';
+let 
+    startCalc = document.querySelector('#start'),
+    budget = document.getElementsByClassName('budget-value')[0],
+    daybudget = document.getElementsByClassName('daybudget-value')[0],
+    level = document.getElementsByClassName('level-value')[0],
+    expenses = document.getElementsByClassName('expenses-value')[0],
+    optionalexpenses = document.getElementsByClassName('optionalexpenses-value')[0],
+    income = document.getElementsByClassName('income-value')[0],
+    monthsavings = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavings = document.getElementsByClassName('yearsavings-value')[0],
+    expenses1 = document.getElementsByClassName('expenses-item')[0],
+    expenses2 = document.getElementsByClassName('expenses-item')[1],
+    expenses3 = document.getElementsByClassName('expenses-item')[2],
+    expenses4 = document.getElementsByClassName('expenses-item')[3],
+    approve1 = document.getElementsByTagName('button')[0],
+    approve2 = document.getElementsByTagName('button')[1],
+    calc = document.getElementsByTagName('button')[2],
+    oExpenses1 = document.querySelectorAll('.optionalexpenses-item')[0],
+    oExpenses2 = document.querySelectorAll('.optionalexpenses-item')[1],
+    oExpenses3 = document.querySelectorAll('.optionalexpenses-item')[2],
+    chooseIncome = document.querySelector('.choose-income'),
+    checkbox = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+console.log(startCalc);
+console.log(budget);
+console.log(daybudget);
+console.log(level);
+console.log(expenses);
+console.log(optionalexpenses);
+console.log(income);
+console.log(monthsavings);
+console.log(yearsavings);
+console.log(expenses1);
+console.log(expenses2);
+console.log(expenses3);
+console.log(expenses4);
+console.log(approve1);
+console.log(approve2);
+console.log(calc);
+console.log(oExpenses1);
+console.log(oExpenses2);
+console.log(oExpenses3);
+console.log(chooseIncome);
+console.log(checkbox);
+console.log(sum);
+console.log(percent);
+console.log(year);
+console.log(month);
+console.log(day);
+
+
+
+
+
+
+
+
